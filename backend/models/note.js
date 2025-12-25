7cfdd598f1ae694e45bcc49c384c278089abc5fd
David Buller <bullerd@comcast.net>
2025-12-25 13:48:17 -0500

    Part 4 A refactor complete

--- backend/models/note.js
+++ backend/models/note.js
@@ -1,19 +1,5 @@
-//part3c2
+//part4 version - no connection to database; that's in app.js
 const mongoose = require("mongoose");
-//const password = encodeURIComponent(process.argv[2]);
-//const url = `mongodb+srv://davidmbuller_db_user:${password}@cluster0.3tpfwj2.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`;
-const url = process.env.MONGODB_URI;
-
-mongoose.set("strictQuery", false);
-
-mongoose
-  .connect(url, { family: 4 })
-  .then(() => {
-    console.log("connected to MongoDB");
-  })
-  .catch((error) => {
-    console.log("error connecting to MongoDB:", error.message);
-  });
 
 const noteSchema = new mongoose.Schema({
   content: { type: String, minLength: 5, required: true },
